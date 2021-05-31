2abad4d950f5599634e3394c89fac2c68aa2d4c7
Szymon Zygadliński <szygadlinski@gmail.com>
2021-05-31 19:21:21 +0200

    Fix bug with multiple votes from the same IP

--- controllers/photos.controller.js
+++ controllers/photos.controller.js
@@ -1,4 +1,6 @@
+const requestIp = require('request-ip');
 const Photo = require('../models/photo.model');
+const Voter = require('../models/Voter.model');
 
 /****** SUBMIT PHOTO ********/
 
@@ -36,11 +38,9 @@ exports.add = async (req, res) => {
     } else {
       throw new Error('Wrong input!');
     }
-
   } catch(err) {
     res.status(500).json(err);
   }
-
 };
 
 /****** LOAD ALL PHOTOS ********/
@@ -52,7 +52,6 @@ exports.loadAll = async (req, res) => {
   } catch(err) {
     res.status(500).json(err);
   }
-
 };
 
 /****** VOTE FOR PHOTO ********/
@@ -61,8 +60,27 @@ exports.vote = async (req, res) => {
 
   try {
     const photoToUpdate = await Photo.findOne({ _id: req.params.id });
-    if(!photoToUpdate) res.status(404).json({ message: 'Not found' });
-    else {
+    const clientIp = requestIp.getClientIp(req);
+    const voter = await Voter.findOne({ user: clientIp });
+
+    if(!photoToUpdate) {
+      res.status(404).json({ message: 'Not found' });
+    } else if(voter) {
+      if(voter.votes.includes(req.params.id)) {
+        res.status(500).json({ message: 'You already voted for this one' });
+      } else {
+        voter.votes.push(req.params.id);
+        await voter.save();
+        photoToUpdate.votes++;
+        photoToUpdate.save();
+        res.send({ message: 'OK' });
+      }
+    } else {
+      const newVoter = new Voter({
+        user: clientIp,
+        votes: [ req.params.id ], 
+      });
+      await newVoter.save();
       photoToUpdate.votes++;
       photoToUpdate.save();
       res.send({ message: 'OK' });
@@ -70,5 +88,4 @@ exports.vote = async (req, res) => {
   } catch(err) {
     res.status(500).json(err);
   }
-
 };
